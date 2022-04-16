5e430349670970cc49a7da005c3305a395c4267f
Kamil <isaevkamil029@mail.ru>
2022-04-16 21:04:11 +0300

    added the ability to move shapes

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let main = document.querySelector('.main');
 
 let playfield = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -17,11 +19,11 @@ let playfield = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 0, 0, 0, 0],
 ];
+
+let gameSpeed = 400;
 
 function draw() {
     let mainInnerHTML = '';
@@ -29,12 +31,126 @@ function draw() {
         for (let x = 0; x < playfield[y].length; x++) {
         if (playfield[y][x] === 1) {
             mainInnerHTML += '<div class="cell movingCell"></div>';
-        }else {
-            mainInnerHTML += '<div class="cell"></div>'
+        } else if (playfield[y][x] === 2) {
+            mainInnerHTML += '<div class="cell fixedCell"></div>';
+        } else {
+            mainInnerHTML += '<div class="cell"></div>';
         }
     }
 }
 main.innerHTML = mainInnerHTML;
 }
 
+function canTetroMoveDown() {
+    for (let y = 0; y < playfield.length; y++) { 
+        for (let x = 0; x < playfield[y].length; x++) {
+            if (playfield[y][x] === 1) {
+                if (y === playfield.length - 1 || playfield[y + 1][x] === 2) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+function moveTetroDown() {
+    if(canTetroMoveDown()) {
+        for (let y = playfield.length - 1; y >= 0; y--) {
+            for (let x = 0; x < playfield[y].length; x++) {
+                if (playfield[y][x] === 1) {
+                    playfield[y + 1][x] = 1;
+                    playfield[y][x] = 0;
+                }
+            }
+        }
+    }  else {
+        fixTetro();
+    }
+}
+
+
+// ДВИГАЕМ ФИГУРКУ ВЛЕВО
+function canTetroMoveLeft() {
+    for (let y = 0; y < playfield.length; y++) { 
+        for (let x = 0; x < playfield[y].length; x++) {
+            if (playfield[y][x] === 1) {
+                if (x === 0 || playfield[y][x - 1] === 2) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+function moveTetroLeft() {
+    if(canTetroMoveLeft()) {
+        for (let y = playfield.length - 1; y >= 0; y--) {
+            for (let x = 0; x < playfield[y].length; x++) {
+                if (playfield[y][x] === 1) {
+                    playfield[y][x -1] = 1;
+                    playfield[y][x] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+// ДВИГАЕМ ФИГУРКУ ВПРАВО
+function canTetroMoveRight() {
+    for (let y = 0; y < playfield.length; y++) { 
+        for (let x = 0; x < playfield[y].length; x++) {
+            if (playfield[y][x] === 1) {
+                if (x === playfield[0].length - 1) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+function moveTetroRight() {
+    if(canTetroMoveRight()) {
+        for (let y = playfield.length - 1; y >= 0; y--) {
+            for (let x = 0; x < playfield[y].length; x++) {
+                if (playfield[y][x] === 1) {
+                    playfield[y][x -1] = 1;
+                    playfield[y][x] = 0;
+                }
+            }
+        }
+    }
+}
+
+function fixTetro() {
+    for (let y = playfield.length - 1; y >= 0; y--) {
+        for (let x = 0; x < playfield[y].length; x++) {
+            if (playfield[y][x] === 1) {
+                playfield[y][x] = 2;
+            }
+        }
+    }
+
+    playfield[0] = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0];
+    playfield[1] = [0, 0, 0, 1, 1, 1, 0, 0, 0, 0];
+}
+
 draw();
+
+document.onkeydown = function(e) {
+    if (e.keyCode === 37) {
+        moveTetroLeft();
+    } else if (e.keyCode === 39) {
+        moveTetroRight();
+    } else if (e.keyCode === 40) {
+
+    }
+}
+
+function startGame () {
+    moveTetroDown();
+    draw();
+    setTimeout(startGame, gameSpeed);
+}
+
+setTimeout(startGame, gameSpeed);
